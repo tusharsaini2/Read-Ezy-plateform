5fcadd65980c822c6c7401956be52bd95edaa670
Tushar <hellotushar111@gmail.com>
2025-02-07 23:00:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Header from "./component/header";
+import Header from "./component/Header";
 import Footer from "./component/Footer";
 
 function App() {
