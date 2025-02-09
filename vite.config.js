5c06193d132b4c6c90b4bbf825694e95d486f586
Tushar <hellotushar111@gmail.com>
2025-02-09 22:34:20 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/ReadEzy-english-app/',
+  // base:'/ReadEzy-english-app/',
 });
